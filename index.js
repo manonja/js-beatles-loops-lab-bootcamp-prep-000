3d4aa4c4a49d7ad7dea428bb00f9daef27253382
manon jacquin <manon.jacquin@gmail.com>
2018-08-17 14:59:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  let newArray = [];
+  for (let i = 0; i < musicians.length; i++){
+    newArray.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return newArray;
+}
+
+function johnLennonFacts(facts){
+  let i = 0;
+  let newArray = [];
+  while (i < facts.length){
+    newArray.push(facts[i] + "!!!");
+    i++;
+  }
+  return newArray;
+}
+
+
+function increment(num){
+ num++;
+ 
+}
+
+function iLoveTheBeatles(num){
+  let newArray = [];
+  do{
+    newArray.push("I love the Beatles!");
+    increment(num);
+  } while (increment(num) < 15)
+  return newArray;
+}
