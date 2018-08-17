a0023782ba8de82b74843d043ec35ff70b93f5c4
manon jacquin <manon.jacquin@gmail.com>
2018-08-17 15:09:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function increment(num){
 function iLoveTheBeatles(num){
   let newArray = [];
   do{
-    ["I love the Beatles!", ...newArray];
+    ;
     num++;
   } while (num < 15)
   return newArray;
